9b7ea0c53008a3ef25422e412ccf650b7409c75a
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2022-06-21 23:08:53 -0500

    :hammer: set docker compose up database before build ubicor api

--- start.sh
+++ start.sh
@@ -5,27 +5,26 @@ if [ ! -x "$(command -v docker-compose)" ]; then
     alias docker-compose='docker compose'
 fi 
 
-# Pull service images
 echo "\n----------------- INIT PULL SERVICE IMAGES  ---------------------"
 docker-compose pull
 echo "----------------- FINISH PULL SERVICE IMAGES  ---------------------\n"
 
-# build ubicor-api image and its dependencies
+echo "\n----------------- INIT LAUNCH database IMAGES ---------------------"
+docker-compose up -d database
+echo "----------------- FINISH LAUNCH database IMAGES ---------------------\n"
+
 echo "\n----------------- INIT BUILD ubicor-api IMAGES ---------------------"
 docker-compose build --no-cache ubicor-api
 echo "----------------- FINISH BUILD ubicor-api IMAGES ---------------------\n"
 
-# run previews built images
 echo "\n----------------- INIT LAUNCH ubicor-api IMAGES ---------------------"
 docker-compose up -d ubicor-api
 echo "----------------- FINISH LAUNCH ubicor-api IMAGES ---------------------\n"
 
-# build ubicor-frontend image
 echo "\n----------------- INIT BUILD ubicor-frontend IMAGE ---------------------"
 docker-compose build --no-cache ubicor-frontend
 echo "----------------- FINISH BUILD ubicor-frontend IMAGE ---------------------\n"
 
-# run ubicor-frontend image
 echo "\n----------------- INIT LAUNCH ubicor-frontend IMAGES ---------------------"
 docker-compose up -d ubicor-frontend
 echo "----------------- FINISH LAUNCH ubicor-frontend IMAGES ---------------------\n"
